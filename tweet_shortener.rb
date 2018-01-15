bf05c6957ccb6d282f68c1d8779f52f6c29ad44e
apocalypse575 <apocalypse575@gmail.com>
2018-01-15 19:18:38 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -47,7 +47,7 @@ end
 
 def shortened_tweet_truncator(tweet)
     if tweet.length > 140
-      (word_substituter(phrase))[0..140] + "..."
+      word_substituter(phrase)[0..140] + "..."
     else
       tweet
   end.join(" ")
