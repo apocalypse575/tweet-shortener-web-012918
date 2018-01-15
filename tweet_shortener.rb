7caed177c9407ee8cad6cb1e25afe4f68acd07c3
apocalypse575 <apocalypse575@gmail.com>
2018-01-15 19:13:10 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -36,11 +36,9 @@ def bulk_tweet_shortener(tweet)
 end
 
 def selective_tweet_shortener(tweet)
-  tweet.split(" ").map do |phrase|
-    if phrase.length > 140
+    if tweet.length > 140
       word_substituter(phrase)
-    elsif phrase.length < 130
-      binding.pry
+    elsif tweet.length < 130
       phrase
     end
   end
